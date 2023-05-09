4ebb9f80dbe5408cf1f3dfafbc98e0536adc8876
Dion Hermans <d.p.h.hermans@student.tue.nl>
2023-05-09 14:54:47 +0200

    testing

--- userservice/resources/loginapi.py
+++ userservice/resources/loginapi.py
@@ -39,6 +39,35 @@ class LoginAPI:
             }
             return make_response(jsonify(responseObject)), 500
 
+    # def ChangePassword(post_data):
+    #
+    #     try:
+    #         # fetch the user data
+    #         session = Session()
+    #         # check if user already exists
+    #         user = session.query(UserDAO).filter(UserDAO.email == post_data.get('email')).first()
+    #         if user.password == post_data.get('password'):
+    #             user.ChangePassword(post_data.get('new_password'))
+    #             responseObject = {
+    #                 'status': 'success',
+    #                 'message': 'Password succesfully changed.'
+    #             }
+    #             session.close()
+    #             return make_response(jsonify(responseObject)), 200
+    #         else:
+    #             responseObject = {
+    #                 'status': 'fail',
+    #                 'message': 'Incorrect password'
+    #             }
+    #             session.close()
+    #             return make_response(jsonify(responseObject)), 404
+    #     except Exception as e:
+    #         print(e)
+    #         responseObject = {
+    #             'status': 'fail',
+    #             'message': 'Try again'
+    #         }
+    #         return make_response(jsonify(responseObject)), 500
     def ChangePassword(post_data):
 
         try:
@@ -46,21 +75,22 @@ class LoginAPI:
             session = Session()
             # check if user already exists
             user = session.query(UserDAO).filter(UserDAO.email == post_data.get('email')).first()
-            if user.password == post_data.get('password'):
-                user.ChangePassword(post_data.get('new_password'))
-                responseObject = {
-                    'status': 'success',
-                    'message': 'Password succesfully changed.'
-                }
-                session.close()
+            session.close()
+            if user:
+                auth_token = encode_auth_token(user.id)
+                if auth_token:
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token
+                    }
                 return make_response(jsonify(responseObject)), 200
             else:
                 responseObject = {
                     'status': 'fail',
-                    'message': 'Incorrect password'
+                    'message': 'There is no user.',
                 }
-                session.close()
-                return make_response(jsonify(responseObject)), 404
+            return make_response(jsonify(responseObject)), 404
         except Exception as e:
             print(e)
             responseObject = {
