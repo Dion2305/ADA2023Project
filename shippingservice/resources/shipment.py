0c7bf050f6c259a09dd219399f5dcb5fb99c46c4
Marty1913 <martijn.meulendijks@hotmail.nl>
2023-05-15 20:08:33 +0200

    Updated shipping service

--- shippingservice/resources/shipment.py
+++ shippingservice/resources/shipment.py
@@ -34,24 +34,6 @@ class Shipment:
     @staticmethod
     def get_user_data(post_data):
         session = Session()
-        user = post_data.get('user')
-        if 'AUTH_URL' in os.environ:
-            auth_url = os.environ['AUTH_URL']
-        else:
-            auth_url = 'http://accounts_ct:5000/get_user'
-        result = requests.post(auth_url,
-                               headers={'Content-Type': 'application/json',
-                                        'Authorization': auth_header})
-        status_code = result.status_code
-        print(status_code)
-        print(result.json())
-        return [status_code, result.json()]
-
-    @staticmethod
-    def get_shipment(s_id):
-        session = Session()
-        # https://docs.sqlalchemy.org/en/14/orm/query.html
-        # https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_orm_using_query.htm
         shipment = session.query(ShippingDAO).filter(ShippingDAO.id == s_id).first()
 
         if shipment:
@@ -65,5 +47,19 @@ class Shipment:
         else:
             session.close()
             return jsonify({'message': f'There is no shipment with id {s_id}'}), 404
+
+        # session = Session()
+        # user = post_data.get('user')
+        # if 'AUTH_URL' in os.environ:
+        #     auth_url = os.environ['AUTH_URL']
+        # else:
+        #     auth_url = 'http://accounts_ct:5000/get_user'
+        # result = requests.post(auth_url,
+        #                        headers={'Content-Type': 'application/json',
+        #                                 'Authorization': auth_header})
+        # status_code = result.status_code
+        # print(status_code)
+        # print(result.json())
+        # return [status_code, result.json()]
 
 
