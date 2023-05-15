e71776eea4e93452ae71aa15792150c36ac6755b
Marty1913 <martijn.meulendijks@hotmail.nl>
2023-05-15 14:14:36 +0200

    Updated shipping service

--- shippingservice/resources/shipment.py
+++ shippingservice/resources/shipment.py
@@ -9,36 +9,51 @@ class Shipment:
     @staticmethod
     def create_shipment(post_data):
         session = Session()
-        shipment = session.query(ShippingDAO).filter(ShippingDAO.user == authorized_user).first()
-        if not shipment:
-            try:
-                shipment = ShippingDAO(
-                    user = post_data.get('user_id'),
-                    package_id = post_data.get('package_id'),
-                    status = post_data.get('status'))
+        shipment = ShippingDAO(
+            user=post_data.get('user_id'),
+            package_id=post_data.get('package_id'),
+            status=post_data.get('status'))
 
-                session.add(shipment)
-                session.commit()
-                responseObject = {
-                    'status': 'success',
-                    'message': 'Successfully created shipping request.',
-                    'shipment_id': shipment.id
-                    }
-                session.close()
-                return make_response(jsonify(responseObject)), 200
-            except Exception as e:
-                print(e)
-                responseObject = {
-                    'status': 'fail',
-                    'message': 'Some error occurred. Please try again.'
-                }
-                return make_response(jsonify(responseObject)), 401
-        else:
-                responseObject = {
-                    'status': 'fail',
-                    'message': 'Shipment already exists.',
-                }
-                return make_response(jsonify(responseObject)), 202
+        session.add(shipment)
+        session.commit()
+        responseObject = {
+            'status': 'success',
+            'message': 'Successfully created shipping request.',
+            'shipment_id': shipment.id
+        }
+        session.close()
+        return make_response(jsonify(responseObject)), 200
+
+        # shipment = session.query(ShippingDAO).filter(ShippingDAO.user == authorized_user).first()
+        # if not shipment:
+        #     try:
+        #         shipment = ShippingDAO(
+        #             user = post_data.get('user_id'),
+        #             package_id = post_data.get('package_id'),
+        #             status = post_data.get('status'))
+        #
+        #         session.add(shipment)
+        #         session.commit()
+        #         responseObject = {
+        #             'status': 'success',
+        #             'message': 'Successfully created shipping request.',
+        #             'shipment_id': shipment.id
+        #             }
+        #         session.close()
+        #         return make_response(jsonify(responseObject)), 200
+        #     except Exception as e:
+        #         print(e)
+        #         responseObject = {
+        #             'status': 'fail',
+        #             'message': 'Some error occurred. Please try again.'
+        #         }
+        #         return make_response(jsonify(responseObject)), 401
+        # else:
+        #         responseObject = {
+        #             'status': 'fail',
+        #             'message': 'Shipment already exists.',
+        #         }
+        #         return make_response(jsonify(responseObject)), 202
 
 
     @staticmethod
