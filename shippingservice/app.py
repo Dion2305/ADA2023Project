c119ca47810ee87161cd3b389a2d24692b135579
Marty1913 <martijn.meulendijks@hotmail.nl>
2023-05-15 11:54:07 +0200

    Updated shipping service

--- shippingservice/app.py
+++ shippingservice/app.py
@@ -13,26 +13,14 @@ Base.metadata.create_all(engine)
 
 @app.route('/shipments', methods=['POST'])
 def create_shipment():
-    if check_if_authorize(request) == 200:
-        req_data = request.get_json()
-        return Shipment.create(req_data)
-    else:
-        responseObject = {
-            'status': 'fail',
-            'message': 'Try again'
-        }
-        return make_response(jsonify(responseObject)), 401
+    req_data = request.get_json()
+    return Shipment.create(req_data)
 
 
 @app.route('/shipments/<s_id>', methods=['GET'])
 def get_shipment(d_id):
-    if check_if_authorize(request) == 200:
-        return Shipment.get(s_id)
-    else:
-        responseObject = {
-            'status': 'fail',
-            'message': 'Try again'
-        }
-        return make_response(jsonify(responseObject)), 401
+    req_data = request.get_json()
+    return Shipment.get(s_id)
+
 
 app.run(host='0.0.0.0', port=5000, debug=True)
