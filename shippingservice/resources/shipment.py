44323af0f12f1358b17f3f792e92da6e376727e8
Marty1913 <martijn.meulendijks@hotmail.nl>
2023-05-15 20:35:01 +0200

    Updated shipping service

--- shippingservice/resources/shipment.py
+++ shippingservice/resources/shipment.py
@@ -41,7 +41,7 @@ class Shipment:
         else:
             auth_url = 'http://accounts_ct:5000/get_user'
 
-        result = requests.post(auth_url, json={'user': s_id})
+        result = requests.post(auth_url, json={"user": s_id})
 
         status_code = result.status_code
         print(status_code)
