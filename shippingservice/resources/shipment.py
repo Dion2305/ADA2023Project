522b06a0197d0d830ec06e172c76eef7c9461e4b
Marty1913 <martijn.meulendijks@hotmail.nl>
2023-05-15 20:44:21 +0200

    Updated shipping service

--- shippingservice/resources/shipment.py
+++ shippingservice/resources/shipment.py
@@ -47,21 +47,8 @@ class Shipment:
         status_code = result.status_code
         print(status_code)
         session.close()
-        return [status_code, jsonify(result)]
+        return [status_code, result]
 
-        # session = Session()
-        # user = post_data.get('user')
-        # if 'AUTH_URL' in os.environ:
-        #     auth_url = os.environ['AUTH_URL']
-        # else:
-        #     auth_url = 'http://accounts_ct:5000/get_user'
-        # result = requests.post(auth_url,
-        #                        headers={'Content-Type': 'application/json',
-        #                                 'Authorization': auth_header})
-        # status_code = result.status_code
-        # print(status_code)
-        # print(result.json())
-        # return [status_code, result.json()]
 
 
 
